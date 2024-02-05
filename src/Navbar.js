0d57b80dd95ec29c6ef9c0332ea3220f3c52f2d7
Anna Zhykol <annzhy1309@gmail.com>
2024-02-05 19:03:11 +0100

    05.02

---
+++ src/Navbar.js
@@ -0,0 +1,19 @@
+export function Navbar(){
+    return(
+        <div className="navbar">
+             <div>
+        <button></button>
+      </div>
+      <div>
+        <p>Categories</p>
+        <ul>
+          <li>Dresses</li>
+          <li>Tops</li>
+          <li>Pants</li>
+          <li>Shoes</li>
+          <li>Accessories</li>
+        </ul>
+      </div>
+        </div>
+    )
+}
